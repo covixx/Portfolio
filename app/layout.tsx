8c73d6236ee6ef2938b23691127587ac642c4fc8
V <75623407+covixx@users.noreply.github.com>
2024-10-02 21:32:15 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${GeistSans.variable} ${GeistMono.variable} antialiased`}
       >
         {children}
       </body>
